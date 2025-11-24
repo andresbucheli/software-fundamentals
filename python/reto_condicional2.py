dd6db69382ccb793267d5e8d468b98ac83ce66c5
Andres <andresbucheli7.3@gmail.com>
2025-11-24 16:17:42 -0500

    python script del taller entrenamiento reto2

---
+++ python/reto_condicional2.py
@@ -0,0 +1,9 @@
+﻿numero = 0
+print("Introduzca el valor del número: ")
+numero = int(input())
+if abs(numero) % 2 == 0:
+    res = "Par"
+    print("El Numero es: " + res)
+else:
+    res = "Impar"
+    print("El Numero es: " + res)
